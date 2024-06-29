f7123bada4aa93e4470071c9b46555a4c35e7c71
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-29 15:45:56 -0400

    Initial

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"os"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zerologr"
+	"github.com/rs/zerolog"
 )
 
-func main() {
-	if len(os.Args) > 1 && (os.Args[1] == "-version" || os.Args[1] == "version" || os.Args[1] == "-v") {
-		buildInfo := GetBuildInfo()
-		fmt.Println(buildInfo)
-		os.Exit(0)
-	}
+// Logger is a global variable that holds the logger instance
+var Logger logr.Logger
 
-	fmt.Println("Hello, World!")
+func init() {
+	// Initialize the logger
+	zl := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	Logger = zerologr.New(&zl)
+}
+
+func main() {
+	// Create a context with the logger
+	ctx := logr.NewContext(context.Background(), Logger)
+
+	// Call a function that uses the logger
+	doSomething(ctx)
+}
+
+func doSomething(ctx context.Context) {
+	// Retrieve the logger from the context
+	log := logr.FromContextOrDiscard(ctx)
+
+	log.Info("Doing something", "step", 1)
+
+	// Call another function, passing the context
+	doSomethingElse(ctx)
+}
+
+func doSomethingElse(ctx context.Context) {
+	// Retrieve the logger from the context
+	log := logr.FromContextOrDiscard(ctx)
+
+	log.Info("Doing something else", "step", 2)
 }
