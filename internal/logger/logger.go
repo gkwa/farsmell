11ccda5e6ba63900ba83904ac51f3f31b348ab98
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-30 10:08:35 -0400

    stuff

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,42 +1,44 @@
 package logger
 
 import (
-   "context"
-   "os"
-
-   "github.com/go-logr/logr"
-   "github.com/go-logr/zerologr"
-   "github.com/rs/zerolog"
+    "context"
+    "os"
+    "github.com/go-logr/logr"
+    "github.com/go-logr/zerologr"
+    "github.com/rs/zerolog"
 )
 
 var defaultLogger logr.Logger
 
 func init() {
-   defaultLogger = NewLogger(LoggerConfig{UseJSON: false})
+    defaultLogger = NewLogger(LoggerConfig{UseJSON: false, LogLevel: 0})
 }
 
 type LoggerConfig struct {
-   UseJSON bool
+    UseJSON  bool
+    LogLevel int
 }
 
 func NewLogger(config LoggerConfig) logr.Logger {
-   if config.UseJSON {
-   	zl := zerolog.New(os.Stderr).With().Timestamp().Logger()
-   	return zerologr.New(&zl)
-   }
-   zl := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05"}).With().Timestamp().Logger()
-   return zerologr.New(&zl)
+    var zl zerolog.Logger
+    if config.UseJSON {
+        zl = zerolog.New(os.Stderr).With().Timestamp().Logger()
+    } else {
+        zl = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05"}).With().Timestamp().Logger()
+    }
+    zl = zl.Level(zerolog.Level(config.LogLevel))
+    return zerologr.New(&zl)
 }
 
 func NewContext(ctx context.Context) context.Context {
-   return logr.NewContext(ctx, defaultLogger)
+    return logr.NewContext(ctx, defaultLogger)
 }
 
 func FromContext(ctx context.Context) logr.Logger {
-   return logr.FromContextOrDiscard(ctx)
+    return logr.FromContextOrDiscard(ctx)
 }
 
 func WithLogger(ctx context.Context, logger logr.Logger) context.Context {
-   return logr.NewContext(ctx, logger)
+    return logr.NewContext(ctx, logger)
 }
 
--- main.go
+++ main.go
@@ -7,24 +7,26 @@ import (
 )
 
 func main() {
+	// Set log level to Info (0) or Debug (-1) here
 	ctx := logger.NewContext(context.Background())
 	doSomething(ctx)
 
-	jsonLogger := logger.NewLogger(logger.LoggerConfig{UseJSON: true})
+	jsonLogger := logger.NewLogger(logger.LoggerConfig{UseJSON: true, LogLevel: 0})
 	ctx = logger.WithLogger(ctx, jsonLogger)
 	doSomethingWithJSON(ctx)
-
 	doSomethingElse(ctx)
 }
 
 func doSomething(ctx context.Context) {
 	log := logger.FromContext(ctx)
 	log.Info("Doing something", "step", 1)
+	log.V(1).Info("Debug info", "details", "some debug details")
 }
 
 func doSomethingWithJSON(ctx context.Context) {
 	log := logger.FromContext(ctx)
 	log.Info("Doing something with JSON", "step", 2)
+	log.V(1).Info("Debug info in JSON", "details", "some JSON debug details")
 }
 
 func doSomethingElse(ctx context.Context) {
